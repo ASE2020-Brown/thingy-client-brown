e3771b3d7246433d3e5bcbb2ffd8346e015dc03d
joelzubi <joel.zubi@gmail.com>
2020-11-18 20:15:04 +0100

    add template login view

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Login',
+  components: {
+
+  }
+}
+</script>
