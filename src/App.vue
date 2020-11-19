c32bbb543e1638b4d4a4786256b80d5dbc978cab
joelzubi <joel.zubi@gmail.com>
2020-11-19 17:12:59 +0100

    Remove Home dependency.

--- src/App.vue
+++ src/App.vue
@@ -36,17 +36,16 @@
 </template>
 
 <script>
-import Home from './views/Home';
 
 export default {
   name: 'App',
 
   components: {
-    Home,
+
   },
 
   data: () => ({
-    //
+
   }),
 };
 </script>
