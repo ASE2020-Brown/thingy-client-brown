59ce0cd4c888dcde9dcf2cec546fb0d12e93428c
joelzubi <joel.zubi@gmail.com>
2020-12-05 13:27:51 +0100

    Enable login.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,6 +4,7 @@
       <h1>Login</h1>
     </v-card-title>
     <v-card-text>
+
       <v-form>
         <v-text-field
             type="text"
@@ -17,13 +18,17 @@
             :append-icon="visiblePassword ? 'mdi-eye' : 'mdi-eye-off'"
             @click:append="visiblePassword = !visiblePassword"
         ></v-text-field>
+        <v-alert v-if="fail" type="error">
+          Login failed: Wrong username or password!
+        </v-alert>
       </v-form>
+
     </v-card-text>
     <v-divider></v-divider>
     <v-card-actions>
       <p class="my-auto mx-3">No account yet? <a href="/register">Register now</a></p>
       <v-spacer></v-spacer>
-      <v-btn color="info">Login</v-btn>
+      <v-btn color="info" @click="submit">Login</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -38,10 +43,36 @@ export default {
   data: function () {
     return {
       visiblePassword: false,
+
+      token: null,
+
+      fail: false,
     }
   },
   methods: {
+    submit() {
+      this.login();
+      if (!fail) {
+        localStorage.token = this.token;
+        this.$router.push('/');
+      }
+    },
 
+    login() {
+      axios.post('http://localhost:3000/login', {
+            "username": this.username,
+            "password": this.password
+          }
+      ).then(response => {
+        this.fail = false;
+        this.token = response;
+        console.log(response);
+      }).catch(error => {
+        this.fail = true;
+        this.token = null;
+        console.log(error);
+      });
+    },
   },
   mounted() {
 
