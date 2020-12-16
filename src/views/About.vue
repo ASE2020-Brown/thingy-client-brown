02fb28d0fb1e2df48244a93cfc113375a3435aae
joelzubi <joel.zubi@gmail.com>
2020-12-16 18:36:06 +0100

    Update About page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Grandpa Care</h1>
+    <p>Grandpa Care is a project that was created in the course
+    "Advanced Software Engineering" in 2020 by team "brown".</p>
+    <p>It consists of a client and a web server (thingy-api-brown).</p>
+    <p>A connection to a Thingy can be established via MQTT Broker.</p>
   </div>
 </template>
