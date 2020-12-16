5f4b20b0113940f8e5356128cf87f34c753849f8
joelzubi <joel.zubi@gmail.com>
2020-12-16 11:31:13 +0100

    Revert "Delete seperate error messages"
    This reverts commit 68395e8e

---
+++ src/components/AlreadyLoggedIn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-alert type="info">You are logged in as {{ username }}.</v-alert>
+    <v-btn href="/">Home</v-btn>
+    <v-btn color="error" href="/logout">Logout</v-btn>
+  </div>
+</template>
+
+<script>
+import store from "@/store";
+
+export default {
+  name: "AlreadyLoggedIn",
+
+  computed: {
+    username () {
+      return store.state.user.username;
+    }
+  }
+}
+</script>
